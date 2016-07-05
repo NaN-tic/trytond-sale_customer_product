91aa7b5faabe7ccf580ef72c899d06c0838c8e49
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-05 13:24:16 +0200

    Fix on_change_* methods

--- sale.py
+++ sale.py
@@ -21,25 +21,23 @@ class SaleLine:
     @fields.depends('product', 'sale')
     def on_change_product(self):
         ProductCustomer = Pool().get('sale.product_customer')
-        res = super(SaleLine, self).on_change_product()
-        if not self.product:
-            return res
-        party_context = {}
-        if self.sale and self.sale.party:
-            party = self.sale.party
-            if party.lang:
-                party_context['language'] = party.lang.code
-        with Transaction().set_context(party_context):
-            products = ProductCustomer.search([
-                    ('product', '=', self.product.template.id),
-                    ('party', '=', self.sale.party.id),
-                    ])
-        if products:
-            product = products[0]
-            code = product.code or self.product.code
-            name = product.name or self.product.template.name
-            description = name
-            if code:
-                description = '[%s] %s' % (code, description)
-            res['description'] = description
-        return res
+        super(SaleLine, self).on_change_product()
+        if self.product:
+            party_context = {}
+            if self.sale and self.sale.party:
+                party = self.sale.party
+                if party.lang:
+                    party_context['language'] = party.lang.code
+            with Transaction().set_context(party_context):
+                products = ProductCustomer.search([
+                        ('product', '=', self.product.template.id),
+                        ('party', '=', self.sale.party.id),
+                        ])
+            if products:
+                product = products[0]
+                code = product.code or self.product.code
+                name = product.name or self.product.template.name
+                description = name
+                if code:
+                    description = '[%s] %s' % (code, description)
+                self.description = description

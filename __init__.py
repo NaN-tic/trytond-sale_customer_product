eaedb7726396e7ec2c1156b92493c936e97ad0c7
C?dric Krier <ced@b2ck.com>
2018-02-07 18:02:49 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.pool import Pool
 from . import product
 from . import sale
+
 
 def register():
     Pool.register(
--- product.py
+++ product.py
@@ -1,6 +1,6 @@
-#This file is part of sale_customer_product module for Tryton.  The COPYRIGHT
-#file at the top level of this repository contains the full copyright
-#notices and license terms.
+# This file is part of sale_customer_product module for Tryton.  The COPYRIGHT
+# file at the top level of this repository contains the full copyright notices
+# and license terms.
 from trytond.model import ModelView, ModelSQL, fields, Unique
 from trytond.pyson import Eval, If
 from trytond.pool import PoolMeta, Pool

47be4237e87203007b5fb1b53168f378ee133998
Albert Cervera i Areny <albert@nan-tic.com>
2019-07-24 19:11:38 +0200

    In sale.line, do not update description with customer's product name and code, following the guidelines of the sale module in latest versions.

--- sale.py
+++ sale.py
@@ -17,28 +17,3 @@ class SaleLine(metaclass=PoolMeta):
         super(SaleLine, cls).__setup__()
         cls.product.context['sale_customer'] = Eval('_parent_sale',
             {}).get('party')
-
-    @fields.depends('product', 'sale')
-    def on_change_product(self):
-        ProductCustomer = Pool().get('sale.product_customer')
-
-        super(SaleLine, self).on_change_product()
-
-        if self.product and self.sale and self.sale.party:
-            party_context = {}
-            party = self.sale.party
-            if party.lang:
-                party_context['language'] = party.lang.code
-            with Transaction().set_context(party_context):
-                products = ProductCustomer.search([
-                        ('product', '=', self.product.template.id),
-                        ('party', '=', self.sale.party.id),
-                        ])
-            if products:
-                product = products[0]
-                code = product.code or self.product.code
-                name = product.name or self.product.template.name
-                description = name
-                if code:
-                    description = '[%s] %s' % (code, description)
-                self.description = description
